d70e11a97e64890fe8b1891475a9ccbf283352e3
Sourin <iamsourinsarkar@gmail.com>
2023-11-13 19:50:15 +0530

    Fixed the styling issue. New issue: Height of the upper navbar

--- src/components/navigation/Ground.tsx
+++ src/components/navigation/Ground.tsx
@@ -4,11 +4,11 @@ function Ground() {
   return (
     <>
       <div className="h-screen">
-        <div>
+        <div className="h-1/5">
           <Nav_Webview />
         </div>
 
-        <div>
+        <div className="flex-generic h-4/5">
           <div className="">
             <p>
               All the world's a stage, and all the men and women merely players.
