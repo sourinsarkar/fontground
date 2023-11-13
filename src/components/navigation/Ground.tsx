f1410d400687bc98551fed036769fcd855a2a903
Sourin <iamsourinsarkar@gmail.com>
2023-11-13 14:58:35 +0530

    Modified Ground's Structure

--- src/components/navigation/Ground.tsx
+++ src/components/navigation/Ground.tsx
@@ -1,7 +1,15 @@
 function Ground() {
     return (
         <>
-            
+            <div>
+                {/* Background Area */}
+                <div>
+                    {/* Text Area */}
+                    <div>
+                        <p>All the world's a stage, and all the men and women merely players.</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
