ac53991ee253f788b780e1b892374c9f71193783
Sourin <iamsourinsarkar@gmail.com>
2023-11-24 01:43:20 +0530

    Testing: Fonts

--- src/components/playground/Playground.tsx
+++ src/components/playground/Playground.tsx
@@ -1,7 +1,7 @@
 const xyz: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-1/2">
-      <h1 className="mono-font">All the world's a stage, and all the men and women merely players.</h1>
+      <h1 className="sub-font">All the world's a stage, and all the men and women merely players.</h1>
     </div>
   );
 };
