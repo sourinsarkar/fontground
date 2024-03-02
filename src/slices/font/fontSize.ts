9aad51871864341b355bdffb69d55c5c2f8391f2
Sourin <iamsourinsarkar@gmail.com>
2024-03-02 18:49:25 +0530

    Issue: Modularity in Redux Slice Fonts

--- src/slices/font/fontSize.ts
+++ src/slices/font/fontSize.ts
@@ -14,9 +14,24 @@ export const fontSize = createSlice({
     initialState,
     reducers: {
         increment: (state) => {state.value += 1},
-        decrement: (state) => {state.value -= 1;},
+        decrement: (state) => {state.value -= 1},
     },
 });
 
 export const {increment, decrement} = fontSize.actions
-export default fontSize.reducer
+
+const initialState2: fontSizeState = {
+    value: 16,
+}
+
+export const fontSize2 = createSlice({
+    name: "fontSize2",
+    initialState: initialState2,
+    reducers: {
+        increment2: (state) => {state.value += 2},
+        decrement2: (state) => {state.value -= 2},
+    },
+});
+
+export const {increment: increment2, decrement: decrement2} = fontSize.actions
+export default {fontSize: fontSize.reducer, fontSize2: fontSize2.reducer}
